acfbc7743dd56d86b685d3cb178f21e39f47c26d
Lorraine Torres <167276218+lorclau@users.noreply.github.com>
2024-11-07 17:57:31 -0800

    Getting started - button added for Brace dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// todo
+// main.ts
+
+// Create a button element
+const button = document.createElement("button");
+button.textContent = "Click me!";
+
+// Add an event listener
+button.addEventListener("click", () => {
+  alert("You clicked the button!");
+});
+document.body.appendChild(button);
